6e24efd235d054690b434a911d43bbc38936d6ac
Saydam-Dev <emirhannkeles@gmail.com>
2025-09-10 04:07:27 +0300

    Yeni başlangıç: temiz repo

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "/saydam.github.io/",
+  base: "/saydam-dev.github.io/",
   plugins: [
     react(),
     tailwindcss(),
