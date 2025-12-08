b12a81e43c3f68feb381ddcf8e56f43f82e1c96f
Saydam-Dev <emirhannkeles@gmail.com>
2025-12-08 07:18:37 +0300

    Added a link prop to have reusable component link

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,7 +7,7 @@ function Projects(props){
                 <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"/>
                     <div className="z-10 sm:order-2 sm:col-span-6">
                         <h3>
-                            <a className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base" href="www.github.com" rel="noreferrer noopener" aria-label="github projesine gider">
+                            <a className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base" href={props.projelinki} rel="noreferrer noopener" aria-label="github projesine gider">
                                 <div className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"/>
                                 <div>{props.projeismi}</div>
                             </a>
